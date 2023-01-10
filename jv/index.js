9e3dd436a62fc694d5f7fc4bd8b9d44e7a58aec9
AneeqArhad <122408112+AneeqArhad@users.noreply.github.com>
2023-01-11 03:11:58 +0500

    Add files via upload

---
+++ jv/index.js
@@ -0,0 +1,32 @@
+//first question Answer
+
+const obj  =new Object();
+obj.name ="Aneeq Arshad";
+obj.age = 19;
+obj.University  = "MNSUAM";
+obj.corse = "BSCS";
+obj.city = "Multan";
+console.log(obj);
+//second question Answer
+
+var obj1 ={
+namee :"Aneeq Arshad",
+age : 19,
+city :"Multan",
+corse : "BSCS",
+}
+console.log(obj1);
+// third question answer function to Display name
+
+function Display(){
+    console.log("Aneeq");
+} 
+    Display();
+    //extra function to add numbers 
+    function Add(a,b,c){
+        console.log(a+b-c);
+        console.log(a*b/c);
+        console.log(a-b-c);
+        console.log(a/b*c);
+    }
+    Add(5,6,5);
